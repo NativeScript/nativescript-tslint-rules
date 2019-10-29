04e137334debb250a37331afdefa569e1ac7c821
vakrilov <alexander.vakrilov@telerik.com>
2019-10-29 11:53:42 +0200

    feat: no-android-resources rule

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ export {
     parseCompilerOptions
 } from "./preferMappedImportsRule";
 
+export { Rule as NoAndroidResourcesRule } from "./noAndroidResourcesRule";
+
 export const rulesDirectory = ".";
 export const rules = {
-    "prefer-mapped-imports": [true]
+    "prefer-mapped-imports": [true],
+    "no-android-resources": [true]
 };
